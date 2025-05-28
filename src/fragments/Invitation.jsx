cc4554a23fb2197e07f24b43544055722213e766
JsCano7 <armando.cano3312@gmail.com>
2025-05-28 14:50:11 -0600

    Update data

--- src/fragments/Invitation.jsx
+++ src/fragments/Invitation.jsx
@@ -7,14 +7,14 @@ const Invitation = () => {
         <div className="invitation">
             <div className="confirm" style={{ paddingRight: "5px" }}>
                 <p>Invitado de la novia</p>
-                <a href="https://api.whatsapp.com/send?phone=+525532083814&text=Confirmo" target="_blank">Confirmar</a>
+                <a href="https://api.whatsapp.com/send?phone=+525547871654&text=Confirmo%20mi%20asistencia" target="_blank">Confirmar</a>
             </div>
             <div className="img">
                 <img src={Img} alt="Detail" data-aos="zoom-in" />
             </div>
             <div className="confirm" style={{ paddingLeft: "5px" }}>
                 <p>Invitado del novio</p>
-                <a href="https://api.whatsapp.com/send?phone=+525547871654&text=Confirmo" target="_blank">Confirmar</a>
+                <a href="https://api.whatsapp.com/send?phone=+525532083814&text=Confirmo%20mi%20asistencia" target="_blank">Confirmar</a>
             </div>
         </div>
     )
